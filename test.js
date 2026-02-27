456c2ba15b9eac73af6346f0e014451f7d6b1b36
Wiktor Owczarek <tikolu43@gmail.com>
2026-02-27 15:10:58 +0000

    Add deno/node test

---
+++ test.js
@@ -0,0 +1,5 @@
+import * as symbols from "./index.js"
+
+console.log("Codepoint:", symbols.iconCodepoint("heart_smile"))
+console.log("Symbol:", symbols.iconSymbol("heart_smile"))
+console.log("Entity:", symbols.iconEntity("heart_smile"))
